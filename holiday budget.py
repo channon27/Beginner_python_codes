e03d936a86c6f7c7c180b9177d7fc7996fb65df6
channon27 <fz72n4g9nb@privaterelay.appleid.com>
2026-03-13 19:31:47 +0000

    Refactor holiday budget input handling with error checks

--- holiday budget.py
+++ holiday budget.py
@@ -1,40 +1,44 @@
-# Currency converter
 valid = True
+valid2 = True
 while valid == True:
     opt = input("Convert and budget (1) or just budget (2) (Enter '1' or '2'): ")
     if opt == "1":
-        money = float(input("Enter amount of money you have remaining (GBP): "))
-        days = float(input("Enter days left on holiday: "))
-        print (money/days*1.36, "USD per day")
-        Try_again = False
-        while True:
-            try2 = input("Would you like to play again? Yes (1) No (2) ")
-            if try2 =="1":
-                break
-            elif try2 =="2":
-                print("Goodbye!")
-                exit()
-            else:    
-                print("Error! Enter 1 or 2. Try again")
-                
-    elif opt == "2":
-        money2 = float (input("Enter amount of money you have remaining (USD): "))
-        days2 = float(input("Enter days left on holiday: "))
-        print (money2/days2,"USD per day")
-        Try_again = False
-        while True:
-            try2 = input("Would you like to play again? Yes (1) No (2) ")
-            if try2 =="1":
-                break
-            elif try2 =="2":
-                print("Goodbye!")
-                exit()
-            else:    
-                print("Error! Enter 1 or 2. Try again")
-
-
-
+        try:
+            money = float(input("Enter amount of money you have remaining (GBP): "))
+            days = float(input("Enter days left on holiday: "))
+            print (money/days*1.36, "USD per day")
+            while True:
+                try2 = input("Would you like to play again? Yes (1) No (2) ")
+                if try2 =="1":
+                    break
+                elif try2 =="2":
+                    print("Goodbye!")
+                    valid = False
+                    break
+                else:    
+                    print("Error! Enter 1 or 2. Try again")
+        except:
+                print("Error! Enter a valid input")
+                valid = True            
     
-
-
-
+    elif opt == "2":
+            while valid2 == True:
+                try:
+                    money2 = float (input("Enter amount of money you have remaining (USD): "))
+                    days2 = float(input("Enter days left on holiday: "))
+                    print (money2/days2,"USD per day")
+                    while True:
+                        try3 = input("Would you like to play again? Yes (1) No (2) ")
+                        if try3 =="1":
+                            valid2 = True
+                            break
+                        elif try3 =="2":
+                            print("Goodbye!")
+                            valid2 = False
+                            valid = False
+                            break
+                        else:    
+                            print("Error! Enter 1 or 2. Try again")
+                except:
+                        print("Error! Enter a valid input")
+                        valid = True          
